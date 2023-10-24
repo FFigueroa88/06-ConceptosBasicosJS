d543d4f30e021e5622f6c91fbbe282c9a6ddd469
Figueroa Fernando <feroa88@gmail.com>
2023-10-23 22:47:02 -0300

    primera clase de javascript como declarar variables y como mostrar mensajes por consola

---
+++ js/01-variables.js
@@ -0,0 +1,14 @@
+//esto es un comentario de una  unica linea 
+/*
+esto es un comentario de multiples lineas 
+*/
+
+// declarar una variable (var - let- const)
+
+let anioActual = 2023;
+
+// Mostrar Mensajes 
+
+console.log(anioActual);
+console.log("anioActual");
+console.log("Año Actual: " + anioActual);
