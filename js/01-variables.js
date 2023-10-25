8c56dd7b028a4a52f79aa0ede94cb379faf745f2
Figueroa Fernando <feroa88@gmail.com>
2023-10-25 17:22:29 -0300

    agregamos mensajes con document wirte y alert

--- js/01-variables.js
+++ js/01-variables.js
@@ -4,11 +4,19 @@ esto es un comentario de multiples lineas
 */
 
 // declarar una variable (var - let- const)
-
 let anioActual = 2023;
+const universe = "https://universe.rollingcodeschool.com"
 
 // Mostrar Mensajes 
-
 console.log(anioActual);
 console.log("anioActual");
 console.log("Año Actual: " + anioActual);
+console.log(universe);
+
+// modificar el contenido de una variable 
+anioActual = 2024 ;
+console.log(anioActual);
+document.write("<p class='parrafoDestacado'>Esto es una prueba de document.write</p>");
+
+// ventanas emergentes 
+alert("Hola Mundo"); 
