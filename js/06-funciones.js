b502572069bbeb819de89c3111b8a3b21afd4ce9
Figueroa Fernando <feroa88@gmail.com>
2023-11-08 18:33:13 -0300

    se empezo el tema funciones

---
+++ js/06-funciones.js
@@ -0,0 +1,25 @@
+// funciones declrativas
+function saludar(){
+    //todo el codigo que sabe hacer la funcion
+    document.write(`<p>HOLA MUNDO</p>`);
+}
+function saludoNuevo(nombreSolicitado , apellidoSolicitado){
+
+    document.write(`<p>HOLA ${nombreSolicitado},${apellidoSolicitado} 🤖</p>`);
+
+}
+function multiplicar(numero1,numero2){
+    const resultado = numero1*numero2;
+    return resultado;
+
+}
+//  llamar  o invocar a la funcion 
+saludar();
+
+const nombre = prompt(`Ingrese un nombre`);
+const apellido = prompt(`Ingrese un apellido`);
+saludoNuevo(nombre,apellido);
+
+const resultadoNuevo = multiplicar(30,3);
+
+document.write(`<p>El resutlado del multiplicacion fue: ${resultadoNuevo} </p>`);
