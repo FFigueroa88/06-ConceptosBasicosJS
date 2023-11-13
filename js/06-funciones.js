e20769a7f067a671c80ff230eb6918964fb72bf7
Figueroa Fernando <feroa88@gmail.com>
2023-11-13 17:16:14 -0300

    agregamos un ejemplo de funcion flecha

--- js/06-funciones.js
+++ js/06-funciones.js
@@ -1,25 +1,28 @@
 // funciones declrativas
-function saludar(){
-    //todo el codigo que sabe hacer la funcion
-    document.write(`<p>HOLA MUNDO</p>`);
+function saludar() {
+  //todo el codigo que sabe hacer la funcion
+  document.write(`<p>HOLA MUNDO</p>`);
 }
-function saludoNuevo(nombreSolicitado , apellidoSolicitado){
-
-    document.write(`<p>HOLA ${nombreSolicitado},${apellidoSolicitado} 🤖</p>`);
-
+function saludoNuevo(nombreSolicitado, apellidoSolicitado) {
+  document.write(`<p>HOLA ${nombreSolicitado},${apellidoSolicitado} 🤖</p>`);
 }
-function multiplicar(numero1,numero2){
-    const resultado = numero1*numero2;
-    return resultado;
-
+function multiplicar(numero1, numero2) {
+  const resultado = numero1 * numero2;
+  return resultado;
 }
-//  llamar  o invocar a la funcion 
+// arrow function
+
+const muliplicar2 = (numero1, numero2) =>  numero1 * numero2;
+  
+//  llamar  o invocar a la funcion
 saludar();
 
 const nombre = prompt(`Ingrese un nombre`);
 const apellido = prompt(`Ingrese un apellido`);
-saludoNuevo(nombre,apellido);
+saludoNuevo(nombre, apellido);
 
-const resultadoNuevo = multiplicar(30,3);
+const resultadoNuevo = multiplicar(30, 3);
 
-document.write(`<p>El resutlado del multiplicacion fue: ${resultadoNuevo} </p>`);
+document.write(
+  `<p>El resutlado del multiplicacion fue: ${resultadoNuevo} </p>`
+);
