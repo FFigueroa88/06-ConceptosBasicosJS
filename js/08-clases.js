51ffc53d3d690a5e087b3d54750e5990bfc69023
Figueroa Fernando <feroa88@gmail.com>
2023-11-22 18:20:09 -0300

    agregamos un ejemplo de polimorfismo

--- js/08-clases.js
+++ js/08-clases.js
@@ -33,6 +33,52 @@ class Pokemon {
         </ul>`);
     }
 }
+
+class PokemonLegendario extends Pokemon{
+    #rareza;
+    #ataqueEspecial;
+    constructor(nombre , tipo, peso, altura, evolucion , ataques, numPokedex, fuerza,rareza,ataqueEspecial){
+        super();
+        this.#rareza =  rareza;
+        this.#ataqueEspecial = ataqueEspecial;
+    }
+    get rareza(){
+        return this.#rareza;
+    }
+    set rareza(nuevaRareza){
+        if(nuevaRareza.length >= 3 && nuevaRareza.length <=10){
+            this.#rareza = nuevaRareza;
+        }else{
+            alert(`dato no válido`);
+        }
+        
+    }
+    get ataqueEspecial(){
+        return this.#ataqueEspecial;
+    }
+    set ataqueEspecial(nuevoataqueEspecial){
+            this.#ataqueEspecial = nuevoataqueEspecial;
+    }
+    // agregar mas metodos
+    mostrarDatosnuevos(){
+
+    }
+    // polimorfismo
+    mostrarDatos(){
+        document.write(`<h2>Pokemon: ${this.nombre} </h2>
+        <ul>
+            <li>Tipo: ${this.tipo}</li>
+            <li>Peso: ${this.peso}</li>
+            <li>Altura: ${this.altura}</li>
+            <li>Evolución: ${this.evolucion}</li>
+            <li>Nro en Pokedex: ${this.numPokedex}</li>
+            <li>Rareza: ${this.rareza}</li>
+            <li>Ataque Especial: ${this.ataqueEspecial}</li>
+        </ul>`);
+
+    }
+
+}
 // crear o instanciar un objeto 
 const charmander = new Pokemon("Charmander","fuego",15,1.20,["Charmeleon","Charizard"],["blaze","ascuas","lanzallamas"],4,4);
 const togepi = new Pokemon("Togepi","hada",1.5,0.3,["Togetic","Togekiss"],["gracia serena","dicha y entusiasmo","placaje"],175,2);
@@ -46,3 +92,7 @@ document.write(`<p>Tipo: ${charmander.tipo} </p>`);
 
 charmander.mostrarDatos();
 togepi.mostrarDatos();
+
+const mewtwo = new PokemonLegendario("Mewtwo","psiquico",122,2,["no evoluciona"],["placaje","confusion","Siniestro"],150,7,"Infrecuente",["bola sombra","charge bean"]);
+mewtwo.mostrarDatos();
+
